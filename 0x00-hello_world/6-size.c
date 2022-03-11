3ea89a0fbd46fd510cf0b3a9a9ad011090010d81
Arslan23 <expeditaza@gmail.com>
2022-03-11 04:50:33 -0800

    Prints the type of the various type on computer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,8 +9,8 @@ int main(void)
 {
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
-	printf("Sizeof a long int: %li byte(s)\n", sizeof(long int));
-	printf("Sizeof a long long int: %lli byte(s)\n", sizeof(long long int));
-	printf("Sizeof a float: %f byte(s)\n", sizeof(float));
+	printf("Sizeof a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Sizeof a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Sizeof a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
