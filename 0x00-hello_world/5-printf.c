21e17afa42331a0c0f954b8aa20e73661c6cc4db
Arslan23 <expeditaza@gmail.com>
2022-03-11 03:08:07 -0800

    Saving

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,17 +1,12 @@
+
 #include <stdio.h>
 /**
-
- * main - Use printf to display message to screen
- *
-
+ *  main  - Display message to screen with  printf function
  * Return: Always 0 (Success)
-
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
-
-	return (0);
-	
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);	
 }
