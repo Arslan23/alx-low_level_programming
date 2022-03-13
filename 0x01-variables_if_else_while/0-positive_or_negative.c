3db3e82fe6c97f66f88729ae5338f6596825f9ac
Arslan23 <expeditaza@gmail.com>
2022-03-13 06:23:32 -0700

    Saving changes

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,12 +1,11 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
 /**
-*main - Print value if is negative or positive
-*
-*Return : Always 0 (Success)
+* main - Print value if is negative or positive
+* Description: Print in standard output if n is positive, zero or negative
+* Return: Always return 0 (Success)
 */
-
 int main(void)
 {
 	int n;
