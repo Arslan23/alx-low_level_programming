a3d794c41215ecc44cb864a771df4ea65c63830c
Arslan23 <expeditaza@gmail.com>
2022-09-03 09:55:44 +0100

    Task10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+
+/**
+*main - Print all combinations two digits
+*Description: Print all combinations two digits
+*Return: Always return 0 (Success)
+*/
+
+
+int main(void)
+{
+
+
+int ch;
+int ch2;
+int tmp = 0;
+
+for (ch = 48; ch <= 57; ch++)
+{	tmp = ch;
+	for (ch2 = 48;  ch2 <= 57 ; ch2++)
+	{
+		if (tmp < ch2)
+		{
+			putchar(ch);
+			putchar(ch2);
+			if (tmp == 56)
+			{
+				putchar(' ');
+			}
+			else
+			{
+				putchar(',');
+			}
+		}
+	}
+}
+return (0);
+}
